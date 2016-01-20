c1ec00bd57a1bf755db22a7c77b4ec97074c84ba
Nick Farrow <farrow.nick@gmail.com>
2016-01-20 00:53:59 -0600

    Script to generate and distribute ssh-keys
    Run this script to generate and distribute ssh-keys. You must place
    ssh_keymaker.sh and NodeList in your home directory.

---
+++ ssh_keymaker.sh
@@ -0,0 +1,18 @@
+#SSH key maker script 
+#This script is a ssh key generator and distributor
+#created by Nick Farrow
+#https://github.com/VeggieVampire/SSH-Key-Helper
+#creates key
+ssh-keygen -t rsa
+#Place NodeList into /home/'your user directory'
+for x in `cat /home/$USER/NodeList`
+do
+        echo "$x"
+        #Makes a ssh directory under your home directory
+		echo "making a .ssh directory for your keys"
+        ssh –q $USER@$x mkdir -p .ssh
+        #places the key you generated above onto the server
+        cat .ssh/id_rsa.pub | ssh $USER@$x 'cat >> .ssh/authorized_keys'
+        #checks if connection works
+        ssh –q $USER@$x 'echo $HOSTNAME'
+done
