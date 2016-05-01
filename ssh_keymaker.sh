9baba87012de0b3aca75ff6fc83c0e4b557cb47f
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-04-30 19:29:56 -0500

    added check access script

--- ssh_keymaker.sh
+++ ssh_keymaker.sh
@@ -10,9 +10,12 @@ do
         echo "$x"
         #Makes a ssh directory under your home directory
 		echo "making a .ssh directory for your keys"
-        ssh –q $USER@$x mkdir -p .ssh
+        ssh â€“q $USER@$x mkdir -p .ssh
         #places the key you generated above onto the server
         cat .ssh/id_rsa.pub | ssh $USER@$x 'cat >> .ssh/authorized_keys'
         #checks if connection works
-        ssh –q $USER@$x 'echo $HOSTNAME'
+        ssh â€“q $USER@$x 'echo $HOSTNAME'
 done
+
+echo "..............Checking access...................."
+./access_check.sh
