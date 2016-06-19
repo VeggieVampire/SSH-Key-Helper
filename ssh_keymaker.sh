491142cac00463b2a8e986f379162220f525fe9e
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-06-19 13:00:51 -0500

    updates
    missing the home directory ~/

--- ssh_keymaker.sh
+++ ssh_keymaker.sh
@@ -12,7 +12,7 @@ do
 		echo "making a .ssh directory for your keys"
         ssh –q $USER@$x mkdir -p .ssh
         #places the key you generated above onto the server
-        cat .ssh/id_rsa.pub | ssh $USER@$x 'cat >> .ssh/authorized_keys'
+        cat ~/.ssh/id_rsa.pub | ssh $USER@$x 'cat >> .ssh/authorized_keys'
         #checks if connection works
         ssh –q $USER@$x 'echo $HOSTNAME'
 done
